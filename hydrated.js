42db55fa2a9613f1c02295d06ab0e469b45e8629
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-05 19:25:58 -0500

    added new js file

---
+++ hydrated.js
@@ -0,0 +1,5 @@
+// to round it to the smallest intenger you would have to use math.floor function
+function litres(time) {
+  // math.floor function returns a intenger that is less than or equal to a given number
+  return Math.floor(time * 0.5);
+}
